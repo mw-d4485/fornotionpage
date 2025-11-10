e730a29d4fdc55c1aff2f4a27045c9e1a6ce8a2e
mw-d4485 <mywang1112@gmail.com>
2025-11-10 17:41:08 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -5,10 +5,10 @@ const ORIGIN = 'https://harmless-shawl-5df.notion.site';
 export function middleware(req) {
   const url = new URL(req.url);
   // 1) Prove it works:
-  return NextResponse.redirect(ORIGIN + url.pathname + url.search, 302);
+  // return NextResponse.redirect(ORIGIN + url.pathname + url.search, 302);
 
   // 2) After you see the 302 working, change the line above to:
-  // return NextResponse.rewrite(ORIGIN + url.pathname + url.search);
+  return NextResponse.rewrite(ORIGIN + url.pathname + url.search);
 }
 
 export const config = {
